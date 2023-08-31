bfa21b317fa66f2e0d3e37b04a83d51370d129fc
BillStephens2022 <two4onebill@yahoo.com>
2023-08-31 14:07:59 -0400

    added ShoppingList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ShoppingList from './components/ShoppingList';
 import './App.css';
 
+interface Item {
+  id: number;
+  product: string;
+  quantity: number;
+}
+
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  // const items = [
+  //   {
+  //     id: 1,
+  //     product: "Lemon",
+  //     quantity: 3
+  //   },
+  //   {
+  //     id: 2,
+  //     product: "Chicken Breast",
+  //     quantity: 2
+  //   }
+  // ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ShoppingList items={items} />
     </div>
   );
 }
